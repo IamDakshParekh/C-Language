2478e567b1cef62612bb80eac4cf5ee14a643962
IamDakshParekh <dakshparekh42@gmail.com>
2024-03-11 19:01:34 +0530

    Without Space Patterns

---
+++ 7.1/Q-6.c
@@ -0,0 +1,33 @@
+/*6 Write a Program to print the below pattern using nested for loop.
+1 0 1 0 1
+1 0 1 0
+1 0 1
+1 0 
+1
+*/
+#include<stdio.h>
+
+main()
+{
+	int i,j;
+	
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",j%2);
+		
+			/*	OR
+				
+			if(j%2==0)
+			{
+				printf("0 ");		
+			}
+			else
+			{
+				printf("1 ");
+			}  */
+		}
+		printf("\n");
+	}
+}
