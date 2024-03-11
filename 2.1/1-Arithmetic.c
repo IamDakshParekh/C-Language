4f1c4cfe7ca3ef4e2c2fdda90ead89f428613cc2
IamDakshParekh <dakshparekh42@gmail.com>
2024-03-11 17:08:32 +0530

    11/03/2024

---
+++ 2.1/1-Arithmetic.c
@@ -0,0 +1,12 @@
+//1. Write a Program to perform all arithmetic operators such as +, -, *, / and %
+#include<stdio.h>
+
+main()
+{
+	int a=12,b=6;
+	printf("Addition of %d and %d is %d\n",a,b,a+b);
+	printf("Subtraction of %d and %d is %d\n",a,b,a-b);
+	printf("Multiplication of %d and %d is %d\n",a,b,a*b);
+	printf("Division of %d and %d is %d\n",a,b,a/b);
+	printf("Modulo of %d and %d is %d\n",a,b,a%b);
+}
